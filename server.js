d6b4852baeebbf192172d734910d21e5c64689fd
MohnadBabgi <mohanadbabgi@gmail.com>
2025-09-21 23:03:19 +0300

    Fixing error with headers

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-
 import http from "node:http";
-
 const PORT = 8000;
-
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-
     if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
         res.end("<h1> Welcome to the Home Page</h1><p>Go to <a href='/about'>About</a></p>");
     } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
         res.end("<h1> About Page</h1><p>This is a simple Node.js server example.</p>");
     } else {
         res.writeHead(404, { "Content-Type": "text/html" });
